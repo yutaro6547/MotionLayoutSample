c879542e32e96f626c17832d1545a384ee6b2729
zukkey <y_vkwi_27_hunhun@yahoo.co.jp>
2018-07-10 08:27:12 +0900

    Add Basic Smaple

--- app/src/main/java/com/whiskey/zukkey/motionlayoutsample/MainActivity.kt
+++ app/src/main/java/com/whiskey/zukkey/motionlayoutsample/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.whiskey.zukkey.motionlayoutsample
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.motion.MotionLayout
+import android.view.View
+import com.whiskey.zukkey.motionlayoutsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+    var motionLayout: View? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        motionLayout = binding.root
+        val ml = motionLayout as? MotionLayout ?: return
+        if (ml.progress > 0.5f) {
+            ml.transitionToStart()
+        } else {
+            ml.transitionToEnd()
+        }
     }
 }
